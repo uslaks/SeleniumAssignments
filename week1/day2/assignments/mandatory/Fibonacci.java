6f2308da16866e84b522ab54f916589d89e910f2
uslaks <100341377+uslaks@users.noreply.github.com>
2022-08-12 18:24:27 +0530

    Completed till Fibonacci of Day2 Assignments

---
+++ week1/day2/assignments/mandatory/Fibonacci.java
@@ -0,0 +1,43 @@
+package week1.day2.assignments.mandatory;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		// Declaring variables for the initial values of a Fibonacci series
+		int firstNum = 0;
+		int secondNum = 1;
+		int sum = 0;
+		
+		// Printing the first value (i.e. 0) of Fibonacci series
+		System.out.println(firstNum);
+		
+		// Iterating from 1 to 11 to derive the Fibonacci series for second to 11th number
+		for (int i =1; i<11; i++) {
+			
+			if (i == 1) {
+				
+				// Printing the second value (i.e. 1) of Fibonacci series as it is only for the first time
+				System.out.println(secondNum);
+								
+			}
+			
+			else {
+			
+				// Deriving the Fibonacci number by adding the firstNum and secondNum and printing the same
+				sum = firstNum+secondNum;
+				
+				System.out.println(sum);	
+				
+				// Updating the firstNum and secondNum as the two preceding numbers
+				firstNum = secondNum;
+				
+				secondNum = sum;
+				
+			}
+			
+		}
+		
+	}
+
+}
