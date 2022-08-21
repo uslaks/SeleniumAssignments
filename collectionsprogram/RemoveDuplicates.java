b0c80c3e8bc698e08d8af623f006e4bd3a574681
uslaks <100341377+uslaks@users.noreply.github.com>
2022-08-22 02:15:14 +0530

    Add files via upload
    Completed Assignments

--- collectionsprogram/RemoveDuplicates.java
+++ collectionsprogram/RemoveDuplicates.java
@@ -26,35 +26,55 @@ public static void main(String[] args) {
 		Set<Character> dupCharSet = new LinkedHashSet<Character>();
 
 //		Iterate character array and add it into charSet
-		for(char eachChar:character) {
+		for(char eachCharecter:character) {
 
 //		if the character is already in the charSet then, add it to the dupCharSet
-			if(charSet.contains(eachChar)) {
+			if(charSet.contains(eachCharecter)) {
 
-				dupCharSet.add(eachChar);
+				dupCharSet.add(eachCharecter);
 
 			} else {
 
-				charSet.add(eachChar);
+				charSet.add(eachCharecter);
 
 			}		
 
 		}
 		
-		System.out.println("charSet : "+charSet);
+		System.out.println("Initial charSet : "+charSet);
 		System.out.println("dupCharSet : "+dupCharSet);
 
 //		Check the dupCharSet elements and remove those in the charSet
+		for(char eachDupChar:dupCharSet) {
+			
+				if(charSet.contains(eachDupChar)) {
+					
+					charSet.remove(eachDupChar);
+				}
+				
+			}
+			
+		System.out.println("Revised charSet (i.e. after removed elements of dupCharSet) : "+charSet);
 
+		
+		Set<Character> finalCharSet = new LinkedHashSet<Character>();
+		
 //		Iterate using charSet
-
-//		Check the iterator variable isn't equals to an empty space
-
+		for(char eachChar:charSet) {
+			
+//		Check the iterator variable isn't equals to an empty space			
+			if(eachChar != ' ') {
+				
+				finalCharSet.add(eachChar);
+								
+			}
+			
+		}
+		
 //		print it
+		System.out.println("The finalCharSet without empty space is : "+finalCharSet);
 
 	}
 
-
-	
 
 }
