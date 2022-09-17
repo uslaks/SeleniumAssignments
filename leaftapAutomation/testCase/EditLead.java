ce8c8ec6091183979300d9533b0187960771bfc5
uslaks <100341377+uslaks@users.noreply.github.com>
2022-09-17 20:17:44 +0530

    Add files via upload

---
+++ leaftapAutomation/testCase/EditLead.java
@@ -0,0 +1,25 @@
+package testCase;
+
+//import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import leaftapAutomation.ProjectSpecificMethods;
+
+public class EditLead extends ProjectSpecificMethods{
+	@Test(dependsOnMethods = "testCase.CreateLead.runCreateLead", groups = "Regression")
+	public void runEditLead() throws InterruptedException {
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.name("submitButton")).click();
+}
+
+}
